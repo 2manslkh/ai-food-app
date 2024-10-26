b13f502cc4ec8d872ac46a01e3496563d207ab92
Kenk <kenghin_lim@hotmail.com>
2024-10-26 18:08:45 +0700

    fix: linter

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useSupabase } from "./SupabaseProvider";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
